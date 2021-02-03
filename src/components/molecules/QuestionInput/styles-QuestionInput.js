e1b904dee96166252922e4847fa578645b7054c1
Joanna Wytrzes <joannawytrzes@gmail.com>
2021-02-03 23:10:53 +0100

    Style form elements

---
+++ src/components/molecules/QuestionInput/styles-QuestionInput.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const StyledWrapper = styled.div`
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	margin-bottom: 1rem;
+`;
